ad7468621a5feb72585540a765502be6af8647e0
Mohammed kalasha <mohammedkalasha786@gmail.com>
2026-03-06 11:30:25 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import dayjs from "dayjs";
+import { navLinks, navIcons } from "#constants/index.js";
+
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Md kalasha's Portfolio</p>
+
+        <ul>
+            {navLinks.map((item) => (
+                <li key={item.id}>
+                    <p>{item.name}</p>
+                </li>
+            ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+            {navIcons.map(({id, img}) => (
+                <li key={id}>
+                    <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                </li>
+            ))}
+        </ul>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
