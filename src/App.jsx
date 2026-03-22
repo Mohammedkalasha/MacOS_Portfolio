4817bb5c36a279acff0648a3041be9fda1b23ebf
Mohammed kalasha <mohammedkalasha786@gmail.com>
2026-03-22 15:27:23 +0300

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Navbar from "#components/Navbar.jsx";
+import { Navbar, Welcome, Dock } from "#components";
 
 const App = () => {
   return (
     <main>
-      <Navbar/>
+      <Navbar />
+      <Welcome />
+      <Dock />
     </main>
   );
 };
